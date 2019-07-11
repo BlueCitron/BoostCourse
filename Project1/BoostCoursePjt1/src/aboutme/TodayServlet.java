0b734a114d52e66e4d5fb5bfaa6127b82340ba27
BlueCitron <sds901234@naver.com>
2019-07-11 16:21:41 +0900

    프로젝트1 추가

---
+++ Project1/BoostCoursePjt1/src/aboutme/TodayServlet.java
@@ -0,0 +1,51 @@
+package aboutme;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AboutmeServlet
+ */
+@WebServlet("/today")
+public class TodayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TodayServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+		response.setContentType("text/html; charset=utf-8");		
+		response.getWriter()
+		.append("<a href='/aboutme'><h1>메인페이지</h1></a><br/>")
+		.append("<h1 style='position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);'>")
+		.append("현재시간 : " + dateFormat.format(new Date()) + "</h1>");
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
